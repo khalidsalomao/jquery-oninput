dbc00fd19d7d9f171721889aa0df64436e05d656
Khalid Salomão <khalidsalomao@gmail.com>
2015-03-24 19:29:22 -0300

    Review

--- jquery.oninput.js
+++ jquery.oninput.js
@@ -9,15 +9,16 @@ jQuery.fn.input = function(fn) {
 	var _this = this;   
     if (fn) {
         return _this.bind("input propertychange", function (event) {
-            // If it's the propertychange event, make sure it's the value that changed.
-            if (window.event && event.type == "propertychange" && event.propertyName != "value")
+            // if it's the propertychange event, make sure it's the value that changed.
+            if (window.event && event.type === "propertychange" && event.propertyName !== "value") {
                 return;
+            }
 
-            // Clear any previously set timer before setting a fresh one
+            // clear any previously set timer before setting a fresh one
             window.clearTimeout(_this.data("timeout"));
             _this.data("timeout", setTimeout(function () {
                 fn.call(this, event);
-            }, 1000));
+            }, 350));
         });
     }
 	return _this.trigger('keydown.input');
